a78729d411b82de36caf8143cb0ad484e56440fd
Wayne Woest <wwoest@gmail.com>
2023-03-14 00:26:39 +0200

    Auto-generated commit [ci skip]

--- src/app/shared/services/script.store.ts
+++
@@ -1,8 +0,0 @@
-interface Scripts {
-    name: string;
-    src: string;
-}  
-
-export const ScriptStore: Scripts[] = [
-    {name: 'currencyConverter', src: 'https://www.currency.me.uk/remote/ER-ERC-1.php'}
-];
