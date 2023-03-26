25733a1de35941bfb5fee3b9f9e2f1abf07484ba
Wayne Woest <wwoest@gmail.com>
2023-03-26 19:45:03 +0200

    Firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ServicesComponent } from './services/services.component';
+import { TermsComponent } from './terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ServicesComponent } from './services/services.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { FaqComponent } from './faq/faq.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
+import { TermsComponent } from './terms/terms.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { NgxWhastappButtonModule } from "ngx-whatsapp-button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    ResourcesComponent,
+    FaqComponent,
+    FooterComponent,
+    TermsComponent,
+    ContactFormComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    NgbModule,
+    NgxWhastappButtonModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
