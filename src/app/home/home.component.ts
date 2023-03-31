d564a15a57491f5bb453c142c8e3f5abcf792dc8
Wayne Woest <wwoest@gmail.com>
2023-03-31 08:51:56 +0200

    Home Page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,49 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
-  @ViewChild('carousel', { static: true }) carousel: NgbCarousel;
-
-  paused = false;
-	unpauseOnArrow = false;
-	pauseOnIndicator = false;
-	pauseOnHover = true;
-	pauseOnFocus = true;
-
-  images = [
-    "assets/images/carousel/carousel-1.jpg",
-    "assets/images/carousel/carousel-2.jpg",
-    "assets/images/carousel/carousel-3.jpg"
-  ];
-
-  public myAngularxQrCode = "http://www.rjlaccounting.co.za/e-card.pdf";
-
-  togglePaused() {
-		if (this.paused) {
-			this.carousel.cycle();
-		} else {
-			this.carousel.pause();
-		}
-		this.paused = !this.paused;
-	}
-
-	onSlide(slideEvent: NgbSlideEvent) {
-		if (
-			this.unpauseOnArrow &&
-			slideEvent.paused &&
-			(slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
-		) {
-			this.togglePaused();
-		}
-		if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-			this.togglePaused();
-		}
-	}
-  
+export class HomeComponent {  
 }
