e82c9c0e6499284408e2ef31bcdc9f006405b58c
Wayne Woest <wwoest@gmail.com>
2023-04-08 15:13:04 +0200

    De-cartoon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,12 @@ import { Router } from '@angular/router';
 
 export class HomeComponent {  
 
+  carouselImages: string[] = [
+    "/assets/images/carousel/carousel1.png",
+    "/assets/images/carousel/carousel2.png",
+    "/assets/images/carousel/carousel3.png"
+  ]
+
   constructor(private router: Router){}
 
   goto(url: string){
