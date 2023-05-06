8b29f699062b2e40773327589dd4c61b89dcb60c
Wayne Woest <wwoest@gmail.com>
2023-05-06 13:15:55 +0200

    Progress

--- src/app/shared/contact.service.ts
+++ src/app/shared/contact.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
 
@@ -12,28 +12,14 @@ export class ContactService {
   constructor(private http: HttpClient) { }
 
   emailMessage(input: any) {
-
-     const headers = new HttpHeaders({
-        'responseType': 'text',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-        'Access-Control-Allow-Headers': 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
-        'Content-Type':'application/json; charset=utf-8',
-        'myCustomHeader':'itsolutionstuff.com'
-      });
-
-      const requestOptions = { headers: headers };
-      
-
-    return this.http.post(this.api, input, requestOptions)
+    return this.http.post(this.api, input, { responseType: 'text' })
     .pipe(
       map(
         (response) => {
           if (response) {
             return response;
           } else {
-            return new Error("uh");
+            return new Error("Not able to send e-mail.");
           }
         },
         (error: any) => {
